86162464c2ad781fdd260031bcc99a2f058446a5
deivintir <d4vidyeva@gmail.com>
2020-06-16 13:59:06 +0200

    Created and executed migrations for DB

--- database/migrations/2020_06_16_085017_create_worked_sessions_table.php
+++ database/migrations/2020_06_16_085017_create_worked_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWorkedsTable extends Migration
+class CreateWorkedSessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateWorkedsTable extends Migration
      */
     public function up()
     {
-        Schema::create('workeds', function (Blueprint $table) {
+        Schema::create('worked_sessions', function (Blueprint $table) {
             $table->id();
-            $table->integer('session_id');
             $table->string('status');
             $table->timestamps();
         });
@@ -28,6 +27,6 @@ class CreateWorkedsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('workeds');
+        Schema::dropIfExists('worked_sessions');
     }
 }
