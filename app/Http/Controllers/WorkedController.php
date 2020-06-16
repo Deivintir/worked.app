1f27c87f0efcef4149dadab8c507a1c9d9070176
deivintir <d4vidyeva@gmail.com>
2020-06-16 14:01:08 +0200

    Implemented functions

--- app/Http/Controllers/WorkedController.php
+++ app/Http/Controllers/WorkedController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Worked;
 use Illuminate\Http\Request;
+use App\WorkedSession;
 
 class WorkedController extends Controller
 {
@@ -14,20 +15,11 @@ class WorkedController extends Controller
      */
     public function index()
     {
-        $worked_ini = Worked::where('status', 'start')->orderByDesc('created_at')->first();
-        dump($worked_ini);
-        return view('index', $worked_ini);
+        $current_session = WorkedSession::getCurrentSession();
+
+        return view('index', ['session' => $current_session]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -37,61 +29,21 @@ class WorkedController extends Controller
      */
     public function store(Request $request)
     {
+        if ($request->status == 'start'){
+            $session = new WorkedSession;
+            $session->status = 'start';
+            $session->save();
+        }else{
+            $session = WorkedSession::getCurrentSession();
+            $session->status = $request->status;
+            $session->save();
+        }
+
         $worked = new Worked;
-
+        $worked->session_id = $session->id;
         $worked->status = $request->status;
-
         $worked->save();
-        
 
-
-        return redirect('/');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Worked  $worked
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Worked $worked)
-    {
-        print "Hola soy el Show.";
-    //SELECT * FROM `workeds` WHERE `status` = 'start' ORDER BY `created_at` DESC
-        return view('/')->with('message', 'tu sesion' . '$status');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Worked  $worked
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Worked $worked)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Worked  $worked
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Worked $worked)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Worked  $worked
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Worked $worked)
-    {
-        //
+        return redirect(route('index'));
     }
 }
